fa04c394a441a0707f1264ae8e615783831d827f
matlegli <leglisematias@gmail.com>
2023-10-09 19:59:32 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+const port = 3000;
+app.listen(port, ()=> {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/');
+});
